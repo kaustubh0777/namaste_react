7790e861fc9d1211327c3c7ee00da5e534105e55
blindbuild <blindbuildlabs@gmail.com>
2024-05-03 16:50:44 +0530

    ep-01

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const parent = React.createElement(
     "div",
     { id: "parent" },
     React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag")
+    React.createElement("h2", {}, "I am an h3 tag")
   );
   
   const root = ReactDOM.createRoot(document.getElementById('root'));
